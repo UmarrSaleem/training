2cdf1acfb93ad6f55d0971125dbdb4b532ccb96a
UmarrSaleem <52926898+UmarrSaleem@users.noreply.github.com>
2025-01-13 23:57:02 +0500

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,19 @@
+// console.log('hello ITM');
+// let helloITM = 123.456;
+// helloITM="ABC";
+// console.log(helloITM);
+
+// console.log(typeof helloITM);
+// let valueinnumber = "33";
+// console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// let checkvalue= Boolean (true);
+// console.log(typeof checkvalue);
+// console.log(1+2-"2");
+// console.log(2!==2);
+// const company="Systems"
+// const num = 5000
+// // console.log(company+ "  Have " + num + " Employee ")
+// console.log(`${company.toUpperCase()} have ${num} Employees `)
+console.log(Math.floor(Math.random()*10))
+
